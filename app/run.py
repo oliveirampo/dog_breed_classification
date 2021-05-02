deb0b3363aa662f099cb98423e8be13654faeff2
oliveirampo <marynna.oliveira@hotmail.com>
2021-05-02 20:41:17 +0200

    feat - Update webpage
    - Style webpage.
    - Send uploaded image to backend python script.

--- app/run.py
+++ app/run.py
@@ -1,11 +1,21 @@
+import base64
+
 from flask_bootstrap import Bootstrap
 from flask_flatpages import FlatPages
 from flask import render_template
 from flask_frozen import Freezer
+from flask import redirect
 from flask import request
+from flask import flash
 from flask import Flask
+from flask import make_response
+from flask import jsonify
+from werkzeug.utils import secure_filename
 import json
 import sys
+import os
+from io import BytesIO
+from PIL import Image
 
 
 # Some configuration, ensures
@@ -27,9 +37,33 @@ Bootstrap(app)
 pages = FlatPages(app)
 freezer = Freezer(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def page_index():
+
+    if request.method == 'POST':
+        data_json = request.get_data()
+
+        if data_json:
+            if type(data_json) == bytes:
+                data_json = data_json.decode('utf8')
+
+            img_data = data_json.split(',')[1]
+
+            f = open('/home/marina/lixo/lixo.png', 'wb')
+            f.write(base64.b64decode(img_data))
+            f.close()
+
+            prediction = getPrediction('image')
+
+            return jsonify(prediction)
+
     return render_template('index.html')
+
+
+def getPrediction(image):
+    return [{"breed": "dog1", "percentage": "(0.0 %)"},
+            {"breed": "dog1", "percentage": "(0.0 %)"},
+            {"breed": "dog3", "percentage": "(0.0 %)"}]
 
 
 if __name__ == '__main__':

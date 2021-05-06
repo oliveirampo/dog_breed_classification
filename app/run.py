23bc7d24419fc832539d76232ff5ea43f2f54e08
oliveirampo <marynna.oliveira@hotmail.com>
2021-05-06 22:31:58 +0200

    feat - Upload VGG16 model to server.

--- app/run.py
+++ app/run.py
@@ -10,13 +10,15 @@ from flask import jsonify
 
 from io import BytesIO
 from PIL import Image
+import cv2
 
 import numpy as np
 import json
 import sys
 import os
 
-from tensorflow.keras.applications.resnet50 import preprocess_input
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import load_model
 
@@ -47,7 +49,9 @@ with open(file_name) as file:
 
 model_path = 'app/model'
 model = load_model(model_path)
-# print(model.summary())
+# model = VGG16(weights='imagenet')
+print(model.summary())
+
 
 @app.route('/', methods=['GET', 'POST'])
 def page_index():
@@ -75,16 +79,26 @@ def page_index():
 
 def getPrediction(image_data, categories, model, top=3):
     img = Image.open(BytesIO(base64.b64decode(image_data)))
-    img = img.resize((244, 224))
+    img = img.resize((224, 224))
 
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
 
-    features = model.predict(x)[0]
+    features = model.predict(x)
+
+    if model.name == 'vgg16':
+        results = decode_predictions(features, top=3)
+        results = [{"breed": res[1].replace('_', ' '), 'percentage': '({:.2f} %)'.format(res[2] * 100.0)} for res in
+                   results[0]]
+        return results
+
+    else:
+        features = features[0]
 
     top_indices = features.argsort()[-top:][::-1]
-    results = [{"breed": categories[str(i)].replace('_', ' '), "percentage": '({:.2f} %)'.format(features[i] * 100.0)} for i in top_indices]
+    results = [{"breed": categories[str(i)].replace('_', ' '), "percentage": '({:.2f} %)'.format(features[i] * 100.0)}
+               for i in top_indices]
 
     return results
 
